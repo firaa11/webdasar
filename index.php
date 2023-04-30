3660a30958428bc7df663f2942d25d7a3f62d9ec
Fitri Rahayu <rahayufitri607@gmail.com>
2023-05-01 00:50:16 +0700

    tambah baru index php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
    $nama_kamu = "Fitri Rahayu";
-   $umur = 21;
+   $umur = 22;
    // tanda petik dua 
    /*
    echo "Hello World, Welcome ".$nama_kamu;
@@ -11,9 +11,9 @@
    */
    echo "Hello World, Welcome ".$nama_kamu." Umur Kamu ".$umur." tahun";
    echo "<br/>";
-   echo "Berarti $nama_kamu lahir di tahun";
-   echo 2001-$umur;
-   
+   echo "Berarti $nama_kamu lahir di tahun ";
+   echo 2023-$umur;
+
    // tanda petik satu 
    // echo 'Hello World';
 
