2e606fa95dc7ef916c3639ac6e7cbc2ae428ca36
Fitri Rahayu <rahayufitri607@gmail.com>
2023-05-01 22:13:23 +0700

    ada perubahan tampil-data.php

--- tampil-data.php
+++ tampil-data.php
@@ -2,7 +2,7 @@
 
 $host = "localhost";
 $user = "root";
-$pass = "YES" 
+$pass = "";
 $dbs  = "mahasiswa";
 
 $conn = new mysqli($host, $user, $pass, $dbs);
@@ -11,22 +11,22 @@ if ($conn->connect_error) {
     die("No Connection to Database");
 }
 
-$ql = "SELECT * FROM data_diri";
+$sql = "SELECT * FROM data_diri";
 
-if ($conn->query($sql) === TRUE) {
+if ($conn->query($sql)) {
     $hasil = $conn->query($sql);
 
     if($hasil->num_rows > 0) {
 ?>
 
-       
-       <table border="1" cellpadding="10" cellspacing="0">
-       <?php 
-       while($row = $hasil->fetch_assoc()) {
-       ?>
-       <tr>
-       <?php
-            echo $row['nim']."</td><td>"
+
+        <table border="1" cellpadding="10" cellspacing="0">
+        <?php
+        while($row = $hasil->fetch_assoc()) {
+        ?>
+        <tr>
+        <?php
+             echo "<td>".$row['nim']."</td><td>"
             .$row['nama']."</td><td>"
             .$row['jenis_kelamin']."</td><td>"
             .$row['tempat_lahir']."</td><td>"
@@ -39,7 +39,8 @@ if ($conn->query($sql) === TRUE) {
         ?>
         </table>
         <?php
+
     }
 } else {
     echo "perintah gagal dijalankan";
-} 
+}
